6eac7173044dec0afed422ea92cf0da0ea83c75b
Ridhit Garg <ridhit100@gmail.com>
2021-11-10 21:04:51 -0800

    worked on hand

--- src/hand.jsx
+++ src/hand.jsx
@@ -40,18 +40,60 @@ class Hand extends Component {
 
     render() {
         // iterate through cards and display them. Maybe also display the current point value of the hand.
+        return(
+            <div className="hand">
+              {this.props.cards.map((card, i) => 
+                  <Card suit={card.props.suit}
+                        rank={card.props.rank}
+                        faceDown={card.props.isFaceup}
+                        key={i} />
+              )}
+            </div>
+        );
     }
 
     evaluate() {
         // calculate then return points as an array of integers
+        let hardTotal = 0;
+        let softTotal = 0;
+        for(let card in this.cards){
+            if(card[0] === "T"){
+                hardTotal += 10;
+            }
+            else if(card[0] === "J"){
+                hardTotal += 11;
+                softTotal += 11;
+            }
+            else if(card[0] === "Q"){
+                hardTotal += 12;
+                softTotal += 12;
+            }
+            else if(card[0] === "K"){
+                hardTotal += 13;
+                softTotal += 13;
+            }
+            else if(card[0]=== "A"){
+                hardTotal += 1;
+                softTotal += ((softTotal + 11) > 21 ) ? 1 : 11;
+            }
+            else{
+                hardTotal += parseInt(card[0]);
+                softTotal += parseInt(card[0]);
+            }
+        }
+        let totals = [hardTotal, softTotal];
+        return totals;
     }
 
     add(card) {
         // add the given card to this.cards
+        this.cards.push(card);
     }
 
     clear() {
         // remove all cards from hand (for new game)
+        this.cards = [];
     }
 
 }
+
